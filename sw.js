591fe04f2299bfa9831a57e7d2329d7e8cca540f
vasilije.knezevic <vasilije.upwork@gmail.com>
2021-04-15 15:23:27 +0200

    fc

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./style.css", "./images/icon.png"])
+            return cache.addAll(["./", "./style.css", "./images/WebSearch/WebSearch1.png"])
         })
     )
 })
